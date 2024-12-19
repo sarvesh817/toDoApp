8da1116e2f506ee914ac7ef2c6afdceb0116aa06
Sarvesh <serveshk7@gmail.com>
2024-12-19 20:36:03 +0530

    update file name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
-const dbUrl=mongoose.connect("mongodb://localhost:27017/toDoApp");   
-
+//const dbUrl=mongoose.connect("mongodb://localhost:27017/toDoApp");   
+const dbUrl = mongoose.connect("mongodb+srv://serveshk7:OtjtAGuiBLmayihB@cluster0.yxk0r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 module.exports=dbUrl; 
 
 
