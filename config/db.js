063bce4f8519cd63a8a16c927aa209e5ce2a23f3
Sarvesh <serveshk7@gmail.com>
2024-12-23 13:43:57 +0530

    s

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+require('dotenv').config(); 
 
-//const dbUrl=mongoose.connect("mongodb://localhost:27017/toDoApp");   
-const dbUrl = mongoose.connect("mongodb+srv://serveshk7:OtjtAGuiBLmayihB@cluster0.yxk0r.mongodb.net/toDoApp?retryWrites=true&w=majority&appName=Cluster0");
-module.exports=dbUrl; 
+// Using environment variable for MongoDB URI
+const dbUrl = mongoose.connect(process.env.DB_URI || "mongodb+srv://serveshk7:OtjtAGuiBLmayihB@cluster0.yxk0r.mongodb.net/toDoApp?retryWrites=true&w=majority&appName=Cluster0");
 
-
+module.exports = dbUrl;
