d8919e67b8991945e21d40f39506aca697108913
Sarvesh <serveshk7@gmail.com>
2024-12-24 19:10:28 +0530

    gg

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to the To-Do App!");
 });
-/* app.get("/test", (req, res) => {
+ app.get("/test", (req, res) => {
     res.status(200).send("Server is live!");
 });
-   */
+   
 
 // Listen on port for local development
 const PORT = process.env.PORT || 5000;  // Use the port from environment or 5000 by default
